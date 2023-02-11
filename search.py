b3545add5780b03290e0bc13b1a6d829760d578a
Nep2un <jupi2erinspace@gmail.com>
2023-02-11 15:54:19 -0800

    Add Breadth-first search solution

--- search.py
+++ search.py
@@ -4,22 +4,88 @@ LEFT = 1
 DOWN = 2
 RIGHT = 3
 
+# https://docs.google.com/presentation/d/18_xcUMHzkcqlAaxNcA-3bX4cMFL5vo5U/edit#slide=id.p1
+
+class Node:
+    def __init__(self, row, col, moveToPrevNode, prevNode):
+        self.row = row
+        self.col = col
+        self.moveToPrevNode= moveToPrevNode
+        self.prevNode = prevNode
+
+
 def BFS(puzzle):
-    """
-    Breadth-First Search.
-
-    Arguments:
-    - puzzle: Node object representing initial state of the puzzle
-
-    Return:
-    final_solution: An ordered list of moves representing the final solution.
-    """
-
     final_solution = []
+    q = []
+    visited = [[0,0,0],[0,0,0],[0,0,0]] # 0 = unvisited, 1 = visited
 
-    # TODO: WRITE CODE
+    for r in range(3):
+        for c in range(3):
+            if puzzle[r][c].strip() == "0":
+                startingNode = Node(r, c, None, None)
+                q.append(startingNode)
+                visited[r][c] = 1
+    
+    while not len(q) == 0:
+        currentNode = q.pop(0)
+
+        if currentNode.row == 2 and currentNode.col == 2:
+            final_solution.append(currentNode)
+            break
+        else:
+            next_moves = next_Move(currentNode, visited)
+
+            for move in next_moves:
+                q.append(move)
+                visited[move.row][move.col] = 1
+
+    currentNode = final_solution.pop(0)
+
+    while(currentNode.prevNode != None):
+        final_solution.insert(0, currentNode.moveToPrevNode)
+        currentNode = currentNode.prevNode
 
     return final_solution
+
+
+# BFS helper method: returns coordinates of next possible moves based on location and visited array
+def next_Move(currentNode, visited):
+    possible_moves = [UP, DOWN, LEFT, RIGHT]
+    next_moves = []
+
+    if(currentNode.row==0):
+        possible_moves.remove(UP)
+    
+    if(currentNode.col==0):
+        possible_moves.remove(LEFT)
+    
+    if(currentNode.row==2):
+        possible_moves.remove(DOWN)
+
+    if(currentNode.col==2):
+        possible_moves.remove(RIGHT)
+    
+    for move in possible_moves:
+        if(move == UP and visited[currentNode.row-1][currentNode.col] == 0):
+            nextNode = Node(currentNode.row-1,currentNode.col, DOWN, currentNode)
+            next_moves.append(nextNode)
+            continue
+        if(move == DOWN and visited[currentNode.row+1][currentNode.col] == 0):
+            nextNode = Node(currentNode.row+1, currentNode.col, UP, currentNode)
+            next_moves.append(nextNode)
+            continue
+        if(move == LEFT and visited[currentNode.row][currentNode.col-1] == 0):
+            nextNode = Node(currentNode.row, currentNode.col-1, RIGHT, currentNode)
+            next_moves.append(nextNode)
+            continue
+        if(move == RIGHT and visited[currentNode.row][currentNode.col+1] == 0):
+            nextNode = Node(currentNode.row, currentNode.col+1, LEFT, currentNode)
+            next_moves.append(nextNode)
+            continue
+
+    return next_moves
+
+
 
 
 def DFS(puzzle):
