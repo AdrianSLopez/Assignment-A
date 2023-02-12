3bbb9bb10d78cdf2246a6a349bd2ef892bf01f8a
Nep2un <jupi2erinspace@gmail.com>
2023-02-11 18:45:25 -0800

    Update node constructor with more paramaters, and algorithms

--- search.py
+++ search.py
@@ -5,36 +5,41 @@ DOWN = 2
 RIGHT = 3
 
 class Node:
-    def __init__(self, row, col, moveToPrevNode, prevNode):
+    def __init__(self, row, col, moveToPrevNode=None, prevNode=None, g=0, h=None, f=None, visited = None):
         self.row = row
         self.col = col
         self.moveToPrevNode= moveToPrevNode
         self.prevNode = prevNode
+        self.g = g
+        self.h = h
+        self.f = f
+        self.visited = visited
+        # Each node should have a visited 2d array? 
 
 
 def BFS(puzzle):
     final_solution = []
     q = []
-    visited = [[0,0,0],[0,0,0],[0,0,0]] # 0 = unvisited, 1 = visited
 
     for r in range(3):
         for c in range(3):
             if puzzle[r][c].strip() == "0":
-                startingNode = Node(r, c, None, None)
+                startingNode = Node(r, c, visited=[[0,0,0],[0,0,0],[0,0,0]])
                 q.append(startingNode)
-                visited[r][c] = 1
     
     while not len(q) == 0:
         currentNode = q.pop(0)
-        visited[currentNode.row][currentNode.col] = 1
+        currentNode.visited[currentNode.row][currentNode.col] = 1
+        print("Current Node: [" + str(currentNode.row) + "," + str(currentNode.col) + "]")
 
         if currentNode.row == 2 and currentNode.col == 2:
             final_solution.append(currentNode)
             break
         else:
-            next_moves = next_Move(currentNode, visited)
+            next_moves = next_Move(currentNode)
 
             for move in next_moves:
+                print("Can move to [" + str(move.row) + "," + str(move.col) + "]")
                 q.append(move)
 
     currentNode = final_solution.pop(0)
@@ -46,8 +51,8 @@ def BFS(puzzle):
     return final_solution
 
 
-# BFS helper method: returns coordinates of next possible moves based on location and visited array
-def next_Move(currentNode, visited):
+# BFS/DFS helper method: returns coordinates of next possible moves based on location and visited array
+def next_Move(currentNode):
     possible_moves = [UP, DOWN, LEFT, RIGHT]
     next_moves = []
 
@@ -64,20 +69,20 @@ def next_Move(currentNode, visited):
         possible_moves.remove(RIGHT)
     
     for move in possible_moves:
-        if(move == UP and visited[currentNode.row-1][currentNode.col] == 0):
-            nextNode = Node(currentNode.row-1,currentNode.col, DOWN, currentNode)
+        if(move == UP and currentNode.visited[currentNode.row-1][currentNode.col] == 0):
+            nextNode = Node(currentNode.row-1,currentNode.col, DOWN, currentNode, visited=currentNode.visited)
             next_moves.append(nextNode)
             continue
-        if(move == DOWN and visited[currentNode.row+1][currentNode.col] == 0):
-            nextNode = Node(currentNode.row+1, currentNode.col, UP, currentNode)
+        if(move == DOWN and currentNode.visited[currentNode.row+1][currentNode.col] == 0):
+            nextNode = Node(currentNode.row+1, currentNode.col, UP, currentNode, visited=currentNode.visited)
             next_moves.append(nextNode)
             continue
-        if(move == LEFT and visited[currentNode.row][currentNode.col-1] == 0):
-            nextNode = Node(currentNode.row, currentNode.col-1, RIGHT, currentNode)
+        if(move == LEFT and currentNode.visited[currentNode.row][currentNode.col-1] == 0):
+            nextNode = Node(currentNode.row, currentNode.col-1, RIGHT, currentNode, visited=currentNode.visited)
             next_moves.append(nextNode)
             continue
-        if(move == RIGHT and visited[currentNode.row][currentNode.col+1] == 0):
-            nextNode = Node(currentNode.row, currentNode.col+1, LEFT, currentNode)
+        if(move == RIGHT and currentNode.visited[currentNode.row][currentNode.col+1] == 0):
+            nextNode = Node(currentNode.row, currentNode.col+1, LEFT, currentNode, visited=currentNode.visited)
             next_moves.append(nextNode)
             continue
 
@@ -89,26 +94,25 @@ def next_Move(currentNode, visited):
 def DFS(puzzle):
     final_solution = []
     q = []
-    visited = [[0,0,0],[0,0,0],[0,0,0]]
 
     for r in range(3):
         for c in range(3):
             if puzzle[r][c].strip() == "0":
-                startingNode = Node(r, c, None, None)
+                startingNode = Node(r, c, visited=[[0,0,0],[0,0,0],[0,0,0]])
                 q.insert(0, startingNode)
-                visited[r][c] = 1
 
     while not len(q) == 0:
         currentNode = q.pop(0)
-        visited[currentNode.row][currentNode.col] = 1
-
+        currentNode.visited[currentNode.row][currentNode.col] = 1
+        print("Current Node: [" + str(currentNode.row) + "," + str(currentNode.col) + "]")
         if currentNode.row == 2 and currentNode.col == 2:
             final_solution.append(currentNode)
             break
         else:
-            next_moves = next_Move(currentNode, visited)
+            next_moves = next_Move(currentNode)
 
             for i in range(len(next_moves)):
+                print("Can move to [" + str(next_moves[i].row) + "," + str(next_moves[i].col) + "]")
                 q.insert(i, next_moves[i])
     
     currentNode = final_solution.pop(0)
