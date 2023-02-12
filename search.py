879b603cf544e81839f9b51a4e2d9109ec439a08
Nep2un <jupi2erinspace@gmail.com>
2023-02-11 19:23:03 -0800

    Update A* solution progress

--- search.py
+++ search.py
@@ -14,7 +14,6 @@ class Node:
         self.h = h
         self.f = f
         self.visited = visited
-        # Each node should have a visited 2d array? 
 
 
 def BFS(puzzle):
@@ -70,26 +69,26 @@ def next_Move(currentNode):
     
     for move in possible_moves:
         if(move == UP and currentNode.visited[currentNode.row-1][currentNode.col] == 0):
-            nextNode = Node(currentNode.row-1,currentNode.col, DOWN, currentNode, visited=currentNode.visited)
+            nextNode = Node(currentNode.row-1,currentNode.col, DOWN, currentNode, g=1+currentNode.g, visited=currentNode.visited)
             next_moves.append(nextNode)
             continue
         if(move == DOWN and currentNode.visited[currentNode.row+1][currentNode.col] == 0):
-            nextNode = Node(currentNode.row+1, currentNode.col, UP, currentNode, visited=currentNode.visited)
+            nextNode = Node(currentNode.row+1, currentNode.col, UP, currentNode, g=1+currentNode.g, visited=currentNode.visited)
             next_moves.append(nextNode)
             continue
         if(move == LEFT and currentNode.visited[currentNode.row][currentNode.col-1] == 0):
-            nextNode = Node(currentNode.row, currentNode.col-1, RIGHT, currentNode, visited=currentNode.visited)
+            nextNode = Node(currentNode.row, currentNode.col-1, RIGHT, currentNode, g=1+currentNode.g, visited=currentNode.visited)
             next_moves.append(nextNode)
             continue
         if(move == RIGHT and currentNode.visited[currentNode.row][currentNode.col+1] == 0):
-            nextNode = Node(currentNode.row, currentNode.col+1, LEFT, currentNode, visited=currentNode.visited)
+            nextNode = Node(currentNode.row, currentNode.col+1, LEFT, currentNode, g=1+currentNode.g, visited=currentNode.visited)
             next_moves.append(nextNode)
             continue
 
     return next_moves
 
 
-
+ 
 
 def DFS(puzzle):
     final_solution = []
@@ -125,36 +124,74 @@ def DFS(puzzle):
 
 
 def A_Star_H1(puzzle):
-    """
-    A-Star with Heuristic 1
-
-    Arguments:
-    - puzzle: Node object representing initial state of the puzzle
-
-    Return:
-    final_solution: An ordered list of moves representing the final solution.
-    """
-
     final_solution = []
+    q = []
 
-    # TODO: WRITE CODE
+    for r in range(3):
+        for c in range(3):
+            if puzzle[r][c].strip() == "0":
+                startingNode = Node(r, c, visited=[[0,0,0],[0,0,0],[0,0,0]])
+                q.insert(0, startingNode)
+
+    while not len(q) == 0:
+        currentNode = q.pop(0)
+        currentNode.visited[currentNode.row][currentNode.col] = 1
+        # print("Current Node: [" + str(currentNode.row) + "," + str(currentNode.col) + "]")
+        if currentNode.row == 2 and currentNode.col == 2:
+            final_solution.append(currentNode)
+            break
+        else:
+            next_moves = next_Move(currentNode)
+
+            for move in next_moves:
+                # print("Can move to [" + str(next_moves[i].row) + "," + str(next_moves[i].col) + "]")
+                # find h for each move
+                # calculate f(n) for each move
+                q.append(move)
+
+            # sort based on f(n)
+    
+    currentNode = final_solution.pop(0)
+    
+    while(currentNode.prevNode != None):
+        final_solution.insert(0, currentNode.moveToPrevNode)
+        currentNode = currentNode.prevNode
 
     return final_solution
 
 
 def A_Star_H2(puzzle):
-    """
-    A-Star with Heauristic 2
-
-    Arguments:
-    - puzzle: Node object representing initial state of the puzzle
-
-    Return:
-    final_solution: An ordered list of moves representing the final solution.
-    """
-
     final_solution = []
+    q = []
 
-    # TODO: WRITE CODE
+    for r in range(3):
+        for c in range(3):
+            if puzzle[r][c].strip() == "0":
+                startingNode = Node(r, c, visited=[[0,0,0],[0,0,0],[0,0,0]])
+                q.insert(0, startingNode)
+
+    while not len(q) == 0:
+        currentNode = q.pop(0)
+        currentNode.visited[currentNode.row][currentNode.col] = 1
+        # print("Current Node: [" + str(currentNode.row) + "," + str(currentNode.col) + "]")
+        if currentNode.row == 2 and currentNode.col == 2:
+            final_solution.append(currentNode)
+            break
+        else:
+            next_moves = next_Move(currentNode)
+
+            for move in next_moves:
+                # print("Can move to [" + str(next_moves[i].row) + "," + str(next_moves[i].col) + "]")
+                # find h for each move
+                # calculate f(n) for each move
+                q.append(move)
+
+            # sort based on f(n)
+    
+    currentNode = final_solution.pop(0)
+    
+    while(currentNode.prevNode != None):
+        final_solution.insert(0, currentNode.moveToPrevNode)
+        currentNode = currentNode.prevNode
 
     return final_solution
