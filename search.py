5f4aea4dcd8788ec804a83723dd081710ceda810
Nep2un <jupi2erinspace@gmail.com>
2023-02-20 18:41:10 -0800

    Create and update helper methods. Update BFS search based on new goal state of puzzle being in order and 0 in bottom right.

--- search.py
+++ search.py
@@ -17,31 +17,36 @@ class Node:
         self.puzzle = puzzle
 
 # TODO:
-    # For every algorith switch goal state to perferct puzzle
-
+    # For every algorith switch goal state to perferct puzzle (1/4)
+    # Apply BFS changes to rest of algorithms
 def BFS(puzzle):
+    puzzle = clean(puzzle)
     final_solution = []
-    if(puzzle[2][2].strip() == "0"):
-        return final_solution
-
     q = []
 
+    if(matchGoalState(puzzle)): return final_solution
+
+    # find location of zero, create initial node and append to q
     for r in range(3):
         for c in range(3):
             if puzzle[r][c].strip() == "0":
-                startingNode = Node(r, c, visited=[[0,0,0],[0,0,0],[0,0,0]], puzzle=puzzle)
+                startingNode = Node(r, c, puzzle=puzzle)
                 q.append(startingNode)
     
     while not len(q) == 0:
         currentNode = q.pop(0)
-        currentNode.visited[currentNode.row][currentNode.col] = 1
+        # print(str(currentNode.puzzle[0][0]) + " " + str(currentNode.puzzle[0][1]) + " " +  str(currentNode.puzzle[0][2]))
+        # print(str(currentNode.puzzle[1][0]) + " " + str(currentNode.puzzle[1][1]) + " " +  str(currentNode.puzzle[1][2]))
+        # print(str(currentNode.puzzle[2][0]) + " " + str(currentNode.puzzle[2][1]) + " " +  str(currentNode.puzzle[2][2]))
+        # print()
 
-        if currentNode.row == 2 and currentNode.col == 2:
+        if matchGoalState(currentNode.puzzle):
             final_solution.append(currentNode)
             break
         else:
             next_moves = next_Move(currentNode)
-
+            # print(str(len(next_moves)) + " possibles moves.")
+            # print()
             for move in next_moves:
                 move.puzzle = [[currentNode.puzzle[0][0], currentNode.puzzle[0][1], currentNode.puzzle[0][2]], [currentNode.puzzle[1][0], currentNode.puzzle[1][1], currentNode.puzzle[1][2]], [currentNode.puzzle[2][0], currentNode.puzzle[2][1], currentNode.puzzle[2][2]]]
                 updatePuzzle(currentNode, move)
@@ -157,8 +162,6 @@ def h1(puzzle):
     return misplacedTiles
 
 
-# TODO 
-# Add puzzle to node and update it
 def A_Star_H2(puzzle):
     final_solution = []
     if(puzzle[2][2].strip() == "0"):
@@ -232,7 +235,15 @@ def sort(q):
            
     return q
 
-# Helper method: returns coordinates of next possible moves based on location and visited array
+# Helper method: removes any '\n' from puzzle data
+def clean(puzzle):
+    for r in range(3):
+        for c in range(3):
+            puzzle[r][c] = puzzle[r][c].strip()
+
+    return puzzle
+
+# Helper method: returns list of nodes that contains possible puzzle states where 0 can move based on currentNode's puzzle/zero's position
 def next_Move(currentNode):
     possible_moves = [UP, DOWN, LEFT, RIGHT]
     next_moves = []
@@ -250,39 +261,43 @@ def next_Move(currentNode):
         possible_moves.remove(RIGHT)
     
     for move in possible_moves:
-        nextNode = Node(prevNode=currentNode, g=1+currentNode.g, visited=currentNode.visited)
-
-        # Update 0's new position and move to previous Node
-        if(move == UP and currentNode.visited[currentNode.row-1][currentNode.col] == 0):
+        nextNode = Node(prevNode=currentNode, g=1+currentNode.g)
+        
+        if(move == UP):
             nextNode.moveToPrevNode = DOWN
             nextNode.row = currentNode.row-1
             nextNode.col = currentNode.col
-            next_moves.append(nextNode)
-            continue
-        if(move == DOWN and currentNode.visited[currentNode.row+1][currentNode.col] == 0):
+        elif(move == DOWN): 
             nextNode.moveToPrevNode = UP
             nextNode.row = currentNode.row+1
             nextNode.col = currentNode.col
-            next_moves.append(nextNode)
-            continue
-        if(move == LEFT and currentNode.visited[currentNode.row][currentNode.col-1] == 0):
+        elif(move == LEFT): 
             nextNode.moveToPrevNode = RIGHT
             nextNode.row = currentNode.row
             nextNode.col = currentNode.col-1
-            next_moves.append(nextNode)
-            continue
-        if(move == RIGHT and currentNode.visited[currentNode.row][currentNode.col+1] == 0):
+        else:
             nextNode.moveToPrevNode = LEFT
             nextNode.row = currentNode.row
             nextNode.col = currentNode.col+1
+
+        if(currentNode.prevNode == None):
             next_moves.append(nextNode)
-            continue       
+            continue
+
+        if(nextNode.row == currentNode.prevNode.row and nextNode.col == currentNode.prevNode.col):
+            continue
+
+        next_moves.append(nextNode)
 
     return next_moves
 
+# Helper method : returns true or false if currentNodePuzzle mathes Goal state/puzzle
+def matchGoalState(currentNodePuzzle):
+    goalState = [[1,2,3], [4,5,6], [7,8,0]]
 
-# Compare currentNode's puzzle to goal puzzle return true or false if match
-def matchGoalState(currentNode):
-    goalState = [[1,2,3], [4,5,6], [7,8,9]]
-
-    if(currentNode.)
+    for r in range(3):
+        for c in range(3):
+            if int(currentNodePuzzle[r][c]) != goalState[r][c]:
+                return False
+    
+    return True
